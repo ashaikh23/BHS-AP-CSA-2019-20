fdcb878a293da57d058bfe3c1c4d922b8af5138f
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2020-03-07 11:11:12 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Kelvin k = new Kelvin(500, 0, 0, 0);
+    Fahrenheit f = new Fahrenheit(500, 0, 0, 0);
+		Celsius c = new Celsius(500, 0, 0, 0);
+
+    System.out.println(k);
+    System.out.println(f);
+		System.out.println(c);
+  }
+}
