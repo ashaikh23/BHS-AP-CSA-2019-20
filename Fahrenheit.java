e953a8fc58c523dc04784bdc47b1055fab9d7948
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2020-03-07 11:12:14 -0500

    Update and rename Fahrenheit to Fahrenheit.java

---
+++ Fahrenheit.java
@@ -0,0 +1,26 @@
+public class Fahrenheit extends GenericTemperature {
+	public Fahrenheit(double tK, double tF, double tC, double sT){
+		super(tK, tF, tC, sT);
+		this.startTemp = sT;
+	}
+	
+	public double getK(){
+		temperatureKelvin = startTemp;
+		return temperatureKelvin;
+	}
+	public double getC(){
+		temperatureCelsuis = temperatureKelvin - 273.15;
+		return temperatureCelsuis;
+	}
+	public double getF(){
+		temperatureFahrenheit = (((temperatureKelvin - 273.15)*1.8)+32);
+    //remove this. if no rounding required
+    this.temperatureFahrenheit = Math.round(temperatureFahrenheit);
+    //remove this.
+		return this.temperatureFahrenheit;
+	}
+	
+	public String toString(){
+		return "The temperature for Fahrenheit is " + getK() + " K " + getC() + " C " + getF() + " F";
+	}
+}
