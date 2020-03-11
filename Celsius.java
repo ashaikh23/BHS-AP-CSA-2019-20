7b58e497665cc013e5dfe174fe9686e0eb8a243a
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2020-03-10 21:50:19 -0400

    Update Celsius.java
    submit

--- Celsius.java
+++ Celsius.java
@@ -1,23 +1,18 @@
 public class Celsius extends GenericTemperature {
-	public Celsius(double tK, double tF, double tC, double sT){
-		super(tK, tF, tC, sT);
-		this.startTemp = sT;
+	public Celsius(double sT){
+		super(sT);
 	}
 	
 	public double getK(){
-		temperatureKelvin = startTemp;
-		return temperatureKelvin;
+		return Math.round(startTemp + 273.15);
 	}
+
 	public double getC(){
-		temperatureCelsuis = temperatureKelvin - 273.15;
-		return temperatureCelsuis;
+		return Math.round(startTemp) ;
 	}
+
 	public double getF(){
-		temperatureFahrenheit = (((temperatureKelvin - 273.15)*1.8)+32);
-    //remove this. if no rounding required
-    this.temperatureFahrenheit = Math.round(temperatureFahrenheit);
-    //remove this.
-		return this.temperatureFahrenheit;
+		return Math.round(startTemp * 1.8) +32;
 	}
 	
 	public String toString(){
