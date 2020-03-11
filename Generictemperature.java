1fd1a3a72dd1c9a537dc56182e53c7dc0d2799ee
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2020-03-10 21:27:13 -0400

    Update Generictemperature.java

--- Generictemperature.java
+++ Generictemperature.java
@@ -2,15 +2,12 @@ public abstract class GenericTemperature implements Temperature{
 	double temperatureKelvin;
 	double temperatureFahrenheit;
 	double temperatureCelsuis;
-    double startTemp;
-	
+  	double startTemp;
 	
 	public GenericTemperature(double tK, double tF, double tC, double sT){
-		this.temperatureKelvin = tK;
-		this.temperatureFahrenheit = tF;
-		this.temperatureCelsuis = tC;
-		this.startTemp = sT;
-		
+		temperatureKelvin = tK;
+		temperatureFahrenheit = tF;
+		temperatureCelsuis = tC;
+		this.startTemp = sT;	
 	}
-
 }
