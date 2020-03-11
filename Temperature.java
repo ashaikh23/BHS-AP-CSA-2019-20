e2b0f6ec7c605059ed54ce5e7b2660607644dc4d
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2020-03-10 21:26:40 -0400

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,5 +1,16 @@
 public interface Temperature {
+	/**
+	 * @return temperature in Kelvin
+	 */
 	double getK();
+	
+	/**
+	 * @return temperature in Fahrenheit
+	 */
 	double getF();
+	
+	/**
+	 * @return temperature in Celsius
+	 */
 	double getC();
 }
