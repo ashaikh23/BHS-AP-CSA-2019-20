f1a49e26cfc4687b1fb48648066091a1c77e5efc
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2021-04-01 19:03:23 -0400

    Create Petypes.java

---
+++ Pets Inheritance/Petypes.java
@@ -0,0 +1,18 @@
+public class Petypes{
+
+  public String name;
+  public int lifeSpan;
+  public String breed;
+  public String place;
+  
+  public Petypes(String name, int lifeSpan)
+  {
+    this.name = name;
+    this.lifeSpan = lifeSpan;
+  }
+  
+  public int getlifeSpan(){
+    return lifeSpan;
+  }
+  
+}
