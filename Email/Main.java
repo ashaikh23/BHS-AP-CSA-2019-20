33ba77081cebdebfc18a6d25e1168eb8c729bffc
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2021-04-01 18:53:27 -0400

    Create Main.java

---
+++ Email/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner email = new Scanner(System.in); 
+    Main emailaddress = new Main(user, server, domain);
+  
+  System.out.println("Please enter your email address:");
+  String e = emailaddress.nextLine;
+  
+  
+  System.out.print("User: " + user);
+  System.out.print("Server: "+ server);
+  System.out.print("Domain" + domain);
+  }
+  Scanner update = new Scanner(System.in);
+  System.out.print("Do you want to update your email(Y or N)? ");
+}
